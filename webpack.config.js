f28225b596fb43fe4e8a7dd445afdd65ca233d06
Rajesh-sitaraman <rajesh.sitaraman@sadasystems.com>
2017-01-17 18:24:32 -0800

    Added SPSave

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
+const SPSaveWebpackPlugin = require('spave-webpack-plugin');
+const creds = require("./settings.js");
+
+const siteUrl = "https://sadacloud.sharepoint.com/sites/rajesh";
+const spJSPath = "Style%20Library/rajesh/js";
+const spCSSPath = "Style%20Library/rajesh/CSS";
 
 module.exports = {
     entry: './src/index.js',
@@ -10,6 +16,17 @@ module.exports = {
     devtool: 'source-map',
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
+        new SPSaveWebpackPlugin({
+            "coreOptions": {
+                "checkin": true,
+                "checkinType": 1,
+                "siteUrl": siteUrl
+            },
+            "credentialOptions": creds,
+            "fileOptions": {
+                "folder": spJSPath
+            }
+        })
     ],
     module: {
         rules: [{
